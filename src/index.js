8840bbc20efc2aa18d5c9ba2197cb710ff239ef6
Araujo <antoniojaraujo0@gmail.com>
2022-07-23 12:51:12 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './main.css'
+import { Modal } from './modal'
+import { Sidebar } from './sidebar'
+
+const modalHandler = Modal()
+const sidebarHandler = Sidebar()
+
+modalHandler.init()
+sidebarHandler.init()
